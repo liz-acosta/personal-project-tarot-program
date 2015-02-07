85616a717b21937bc8d4e4995aba7a8e3167be8f
Liz Acosta <lacosta@Lizs-MacBook-Air.local>
2015-02-06 17:52:02 -0800

    Created database and queried from database with Python script

--- 20150203-tarot-deck-sqlite-database-creation.py
+++ 20150203-tarot-deck-sqlite-database-creation.py
@@ -6,7 +6,7 @@ import sqlite3
 DB = None
 CONN = None
 
-tarot_deckF = open("20150203-tarot-deck.tsv")
+tarot_deckF = open("tarot-deck-001.tsv")
 tarot_deck_entry = []
 
 # def deck_from_file(f):
@@ -22,18 +22,19 @@ def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("20150203-tarot-deck-database.db")
     DB = CONN.cursor()
-
+    
 def make_new_entry(file_name):
 	for line in file_name:
 		tarot_deck_entry = line.rstrip().split("	")
-		card_name = tarot_deck_entry[0] 
-		card_meaning = tarot_deck_entry[1] 
-		card_img = tarot_deck_entry[2]
-		query = """INSERT into Tarot values (?, ?, ?)"""
-		DB.execute(query, (card_name, card_meaning, card_img))
+		card_id = tarot_deck_entry[0] 
+		card_name = tarot_deck_entry[1] 
+		card_meaning = tarot_deck_entry[2]
+		card_url = tarot_deck_entry[3]
+		query = """INSERT into Tarot values (?, ?, ?, ?)"""
+		DB.execute(query, (card_id, card_name, card_meaning, card_url))
 
 		CONN.commit()
-		print "Successfully added card: %s %s %s"%(card_name, card_meaning, card_img)
+		print "Successfully added card: %s %s %s"%(card_name, card_meaning, card_url)
 
 def main():
 	connect_to_db()
